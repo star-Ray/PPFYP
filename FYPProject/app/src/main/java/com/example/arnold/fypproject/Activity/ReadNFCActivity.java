ea757ef3e15534c80053dac7d14164f8d8f1fb4b
tweetweetarnold <arnold.lee.wt@gmail.com>
2015-07-28 12:25:52 +0800

    - improve organization of reading and writing nfc messages - improvement to alertdialogs and removed default "nfc off" dialog

--- FYPProject/app/src/main/java/com/example/arnold/fypproject/Activity/ReadNFCActivity.java
+++ FYPProject/app/src/main/java/com/example/arnold/fypproject/Activity/ReadNFCActivity.java
@@ -1,92 +1,60 @@
 package com.example.arnold.fypproject.Activity;
 
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.arnold.fypproject.R;
+import com.nxp.nfclib.classic.MFClassic;
+import com.nxp.nfclib.exceptions.SmartCardException;
+import com.nxp.nfclib.icode.ICodeSLI;
+import com.nxp.nfclib.icode.ICodeSLIL;
+import com.nxp.nfclib.icode.ICodeSLIS;
+import com.nxp.nfclib.icode.ICodeSLIX;
+import com.nxp.nfclib.icode.ICodeSLIX2;
+import com.nxp.nfclib.icode.ICodeSLIXL;
+import com.nxp.nfclib.icode.ICodeSLIXS;
+import com.nxp.nfclib.ntag.NTag;
+import com.nxp.nfclib.ntag.NTag203x;
+import com.nxp.nfclib.ntag.NTag210;
+import com.nxp.nfclib.ntag.NTag213215216;
+import com.nxp.nfclib.ntag.NTag213F216F;
+import com.nxp.nfclib.ntag.NTagI2C;
+import com.nxp.nfclib.plus.PlusSL1;
+import com.nxp.nfclib.ultralight.Ultralight;
+import com.nxp.nfclib.ultralight.UltralightC;
+import com.nxp.nfclib.ultralight.UltralightEV1;
+import com.nxp.nfcliblite.Interface.Inxpnfcliblitecallback;
+import com.nxp.nfcliblite.Interface.NxpNfcLibLite;
+import com.nxp.nfcliblite.cards.DESFire;
+import com.nxp.nfcliblite.cards.Plus;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
+import java.io.IOException;
 
 public class ReadNFCActivity extends ActionBarActivity {
 
-    public static final String MIME_TEXT_PLAIN = "text/plain";
-    public static final String TAG = "NfcDemo";
-
-    private NfcAdapter nfcAdapter;
-    private TextView sampleText;
-
+//    private static final String MIME_TEXT_PLAIN = "text/plain";
+    private NxpNfcLibLite libInstance = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_nfc);
 
-//        initialization
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-//        Stop here if device does not support NFC
-        if (nfcAdapter == null){
-            Toast.makeText(this, "This device does not support NFC.", Toast.LENGTH_LONG).show();
-            finish(); //to end this activity and return
+        if(checkNFCEnabled()){
+            libInstance = NxpNfcLibLite.getInstance();
+            libInstance.registerActivity(this);
         }
-
-//        If NFC setting is not enabled on the device
-        if (!nfcAdapter.isEnabled()){
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle(R.string.dialog_title_nfc_settings_fail).setMessage(R.string.dialog_message_nfc_fail);
-            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int id) {
-                    finish(); //to end this activity and return
-                }
-            });
-            builder.create().show();
-        }
-    }
-
-    protected void handleIntent(Intent intent){
-        String action = intent.getAction();
-        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)){
-            String type = intent.getType();
-            if (MIME_TEXT_PLAIN.equals(type)){
-                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-                new NdefReaderTask().execute(tag);
-            } else{
-                System.out.println("WRONG MIME TYPE!!!");
-            }
-        }else if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
-
-            // In case we would still use the Tech Discovered Intent
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            String[] techList = tag.getTechList();
-            String searchedTech = Ndef.class.getName();
-
-            for (String tech : techList) {
-                if (searchedTech.equals(tech)) {
-                    new NdefReaderTask().execute(tag);
-                    break;
-                }
-            }
-        }
-
     }
 
     @Override
@@ -114,153 +82,166 @@ public class ReadNFCActivity extends ActionBarActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        /**
-         * It's important, that the activity is in the foreground (resumed). Otherwise
-         * an IllegalStateException is thrown.
-         */
-        setupForegroundDispatch(this, nfcAdapter);
-        System.out.println("On Resume Running...");
+        if (libInstance != null){
+            libInstance.startForeGroundDispatch();
+        }
     }
 
     @Override
     protected void onPause() {
-        /**
-         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
-         */
-        stopForegroundDispatch(this, nfcAdapter);
+        if (libInstance != null){
+            libInstance.stopForeGroundDispatch();
+        }
         super.onPause();
+    }
+
+    public void readNTag(NTag tag){
+        try {
+            Toast.makeText(this, "connecting...", Toast.LENGTH_SHORT).show();
+            tag.connect();
+            Toast.makeText(this, "connected", Toast.LENGTH_SHORT).show();
+            NdefMessage message = tag.readNDEF();
+            System.out.println("message: " + message.toString());
+            Toast.makeText(this, "message: " + message.toString(), Toast.LENGTH_SHORT).show();
+            NdefRecord[] records = message.getRecords();
+            byte[] data = records[0].getPayload();
+            String text = new String(data, "UTF-8");
+            System.out.println("message: " + text);
+            Toast.makeText(this, "message: " + text, Toast.LENGTH_SHORT).show();
+
+            tag.close();
+            Toast.makeText(this, "connection close", Toast.LENGTH_SHORT).show();
+
+        } catch (IOException | SmartCardException | FormatException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
-        /**
-         * This method gets called, when a new Intent gets associated with the current activity instance.
-         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
-         * at the documentation.
-         *
-         * In our case this method gets called, when the user attaches a Tag to the device.
-         */
-
-        String showString = "NFC Tag detected!\nIntent: " + intent.toString();
-
-        System.out.println("new intent is here");
-        sampleText = (TextView)findViewById(R.id.nfc_sample_text);
-        sampleText.setText(showString);
-        Toast.makeText(this, showString, Toast.LENGTH_LONG).show();
-//        handleIntent(intent);
-
-        sampleText.setText(intent.getAction());
-        if (intent.getAction().equals(NfcAdapter.ACTION_TAG_DISCOVERED)){
-            Toast.makeText(this,"tag detected!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    /**
-     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
-     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
-     */
-    protected static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
-        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        System.out.println(intent.toString());
-
-        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
-
-        IntentFilter[] filters = new IntentFilter[1];
-        String[][] techList = new String[][]{};
-
-        // Notice that this is the same filter as in our manifest.
-        filters[0] = new IntentFilter();
-        filters[0].addAction(NfcAdapter.ACTION_TAG_DISCOVERED);
-        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
-        try {
-            filters[0].addDataType(MIME_TEXT_PLAIN);
-        } catch (IntentFilter.MalformedMimeTypeException e) {
-            throw new RuntimeException("Check your mime type.");
-        }
-//        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
-        System.out.println("activity: " + activity.toString());
-        System.out.println("pending intent: " + pendingIntent.toString());
-        System.out.println("filters: " + filters.toString());
-        System.out.println("techlist: " + techList.toString());
-        adapter.enableForegroundDispatch(activity, pendingIntent, null, null);
-    }
-
-    /**
-     * @param activity The corresponding {@link BaseActivity} requesting to stop the foreground dispatch.
-     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
-     */
-    protected static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-        adapter.disableForegroundDispatch(activity);
-    }
-
-    /**
-     * Background task for reading the data. Do not block the UI thread while reading.
-     *
-     * @author Ralf Wondratschek
-     *
-     */
-    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
-
-        @Override
-        protected String doInBackground(Tag... params) {
-            Tag tag = params[0];
-
-            Ndef ndef = Ndef.get(tag);
-            if (ndef == null) {
-                // NDEF is not supported by this Tag.
-                return null;
+        libInstance.filterIntent(intent, new Inxpnfcliblitecallback() {
+            @Override
+            public void onUltraLightCardDetected(Ultralight ultralight) {
             }
 
-            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+            @Override
+            public void onClassicCardDetected(MFClassic mfClassic) {
 
-            NdefRecord[] records = ndefMessage.getRecords();
-            for (NdefRecord ndefRecord : records) {
-                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
-                    try {
-                        return readText(ndefRecord);
-                    } catch (UnsupportedEncodingException e) {
-                        Log.e(TAG, "Unsupported Encoding", e);
-                    }
+            }
+
+            @Override
+            public void onDESFireCardDetected(DESFire desFire) {
+
+            }
+
+            @Override
+            public void onUltraLightEV1CardDetected(UltralightEV1 ultralightEV1) {
+
+            }
+
+            @Override
+            public void onUltraLightCCardDetected(UltralightC ultralightC) {
+
+            }
+
+            @Override
+            public void onPlusCardDetected(Plus plus) {
+
+            }
+
+            @Override
+            public void onNTag203xCardDetected(NTag203x nTag203x) {
+            }
+
+            @Override
+            public void onNTag210CardDetected(NTag210 nTag210) {
+            }
+
+            @Override
+            public void onNTag213215216CardDetected(NTag213215216 nTag213215216) {
+                readNTag(nTag213215216);
+            }
+
+            @Override
+            public void onNTag213F216FCardDetected(NTag213F216F nTag213F216F) {
+            }
+
+            @Override
+            public void onNTagI2CCardDetected(NTagI2C nTagI2C) {
+            }
+
+            @Override
+            public void onICodeSLIDetected(ICodeSLI iCodeSLI) {
+
+            }
+
+            @Override
+            public void onICodeSLISDetected(ICodeSLIS iCodeSLIS) {
+
+            }
+
+            @Override
+            public void onICodeSLILDetected(ICodeSLIL iCodeSLIL) {
+
+            }
+
+            @Override
+            public void onICodeSLIXDetected(ICodeSLIX iCodeSLIX) {
+
+            }
+
+            @Override
+            public void onICodeSLIXSDetected(ICodeSLIXS iCodeSLIXS) {
+
+            }
+
+            @Override
+            public void onICodeSLIXLDetected(ICodeSLIXL iCodeSLIXL) {
+
+            }
+
+            @Override
+            public void onPlusSL1CardDetected(PlusSL1 plusSL1) {
+
+            }
+
+            @Override
+            public void onICodeSLIX2Detected(ICodeSLIX2 iCodeSLIX2) {
+
+            }
+        });
+    }
+
+    public boolean checkNFCEnabled(){
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+//        Stop here if device does not support NFC
+        if (nfcAdapter == null){
+            Toast.makeText(this, "This device does not support NFC.", Toast.LENGTH_LONG).show();
+            finish(); //to end this activity and return
+            return false;
+        }
+
+//        Check if NFC is enabled
+        boolean isEnabled = nfcAdapter.isEnabled();
+        if (!isEnabled) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.dialog_title_nfc_settings_fail).setMessage(R.string.dialog_message_nfc_fail);
+            builder.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
                 }
-            }
-
-            return null;
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish(); //to end this activity and return
+                }
+            });
+            builder.create().show();
         }
-
-        private String readText(NdefRecord record) throws UnsupportedEncodingException {
-        /*
-         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
-         *
-         * http://www.nfc-forum.org/specs/
-         *
-         * bit_7 defines encoding
-         * bit_6 reserved for future use, must be 0
-         * bit_5..0 length of IANA language code
-         */
-
-            byte[] payload = record.getPayload();
-
-            // Get the Text Encoding
-            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-
-            // Get the Language Code
-            int languageCodeLength = payload[0] & 0063;
-
-            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
-            // e.g. "en"
-
-            // Get the Text
-            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            if (result != null) {
-                sampleText.setText("Read content: " + result);
-            }
-        }
+        return isEnabled;
     }
 
 }
--- FYPProject/app/src/main/java/com/example/arnold/fypproject/Activity/WriteNFCActivity.java
+++ FYPProject/app/src/main/java/com/example/arnold/fypproject/Activity/WriteNFCActivity.java
@@ -1,17 +1,15 @@
 package com.example.arnold.fypproject.Activity;
 
-import android.app.Activity;
-import android.app.PendingIntent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.nfc.tech.Ndef;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -42,29 +40,23 @@ import com.nxp.nfcliblite.cards.DESFire;
 import com.nxp.nfcliblite.cards.Plus;
 
 import java.io.IOException;
-import java.text.Format;
+import java.nio.charset.Charset;
+import java.text.Normalizer;
 
 public class WriteNFCActivity extends ActionBarActivity {
 
-    public static final String MIME_TEXT_PLAIN = "text/plain";
+    private static final String MIME_PLAIN_TEXT = "text/plain";
     private NxpNfcLibLite libInstance = null;
-    private NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_nfc);
 
-        libInstance = NxpNfcLibLite.getInstance();
-        libInstance.registerActivity(this);
-
-        //        initialization
-//        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-//        //        Stop here if device does not support NFC
-//        if (nfcAdapter == null){
-//            Toast.makeText(this, "This device does not support NFC.", Toast.LENGTH_LONG).show();
-//            finish();
-//        }
+        if(checkNFCEnabled()){
+            libInstance = NxpNfcLibLite.getInstance();
+            libInstance.registerActivity(this);
+        }
     }
 
     @Override
@@ -92,179 +84,119 @@ public class WriteNFCActivity extends ActionBarActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        /**
-         * It's important, that the activity is in the foreground (resumed). Otherwise
-         * an IllegalStateException is thrown.
-         */
-//        setupForegroundDispatch(this, nfcAdapter);
-        libInstance.startForeGroundDispatch();
+        if (libInstance != null){
+            libInstance.startForeGroundDispatch();
+        }
     }
 
     @Override
     protected void onPause() {
-        /**
-         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
-         */
-//        stopForegroundDispatch(this, nfcAdapter);
-        libInstance.stopForeGroundDispatch();
+        if (libInstance != null){
+            libInstance.stopForeGroundDispatch();
+        }
         super.onPause();
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
-        /**
-         * This method gets called, when a new Intent gets associated with the current activity instance.
-         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
-         * at the documentation.
-         *
-         * In our case this method gets called, when the user attaches a Tag to the device.
-         */
-
-//        String showString = "NFC Tag detected!\nIntent: " + intent.toString();
-//        Toast.makeText(this, showString, Toast.LENGTH_LONG).show();
-
-
         libInstance.filterIntent(intent, new Inxpnfcliblitecallback() {
             @Override
             public void onUltraLightCardDetected(Ultralight ultralight) {
             }
+
             @Override
             public void onClassicCardDetected(MFClassic mfClassic) {
 
             }
+
             @Override
             public void onDESFireCardDetected(DESFire desFire) {
 
             }
+
             @Override
             public void onUltraLightEV1CardDetected(UltralightEV1 ultralightEV1) {
 
             }
+
             @Override
             public void onUltraLightCCardDetected(UltralightC ultralightC) {
 
             }
+
             @Override
             public void onPlusCardDetected(Plus plus) {
 
             }
+
             @Override
             public void onNTag203xCardDetected(NTag203x nTag203x) {
             }
+
             @Override
             public void onNTag210CardDetected(NTag210 nTag210) {
             }
 
             @Override
             public void onNTag213215216CardDetected(NTag213215216 nTag213215216) {
-                System.out.println("onNTag213215216CardDetected");
-                System.out.println("tagname: " + nTag213215216.getTagName());
-                System.out.println("type: " + nTag213215216.getType().toString());
-                readNTag(nTag213215216);
-//                writeTag(nTag213215216);
+                writeTag(nTag213215216);
             }
+
             @Override
             public void onNTag213F216FCardDetected(NTag213F216F nTag213F216F) {
             }
+
             @Override
             public void onNTagI2CCardDetected(NTagI2C nTagI2C) {
             }
+
             @Override
             public void onICodeSLIDetected(ICodeSLI iCodeSLI) {
 
             }
+
             @Override
             public void onICodeSLISDetected(ICodeSLIS iCodeSLIS) {
 
             }
+
             @Override
             public void onICodeSLILDetected(ICodeSLIL iCodeSLIL) {
 
             }
+
             @Override
             public void onICodeSLIXDetected(ICodeSLIX iCodeSLIX) {
 
             }
+
             @Override
             public void onICodeSLIXSDetected(ICodeSLIXS iCodeSLIXS) {
 
             }
+
             @Override
             public void onICodeSLIXLDetected(ICodeSLIXL iCodeSLIXL) {
 
             }
+
             @Override
             public void onPlusSL1CardDetected(PlusSL1 plusSL1) {
 
             }
+
             @Override
             public void onICodeSLIX2Detected(ICodeSLIX2 iCodeSLIX2) {
 
             }
         });
-
-//        System.out.println("here1");
-//        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-//        System.out.println(tag.toString());
-//        System.out.println("here2");
-//        System.out.println("here3");
-
-//        try {
-//            System.out.println("test1");
-//            NTag tag = new NTag(){};
-//            System.out.println("test2");
-//            tag.connect();
-//
-//            System.out.println("test3");
-//            System.out.println("Tagname: " + tag.getTagName());
-//
-//            NdefMessage message = tag.readNDEF();
-//            System.out.println("ndefmessage: " + message.toString());
-//
-//        } catch (SmartCardException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (FormatException e) {
-//            e.printStackTrace();
-//        }
-    }
-
-    public void readNTag(NTag tag){
-        try {
-            Toast.makeText(this, "connecting...", Toast.LENGTH_SHORT).show();
-            tag.connect();
-            Toast.makeText(this, "connected", Toast.LENGTH_SHORT).show();
-            NdefMessage message = tag.readNDEF();
-            System.out.println("message: " + message.toString());
-            Toast.makeText(this, "message: " + message.toString(), Toast.LENGTH_SHORT).show();
-            NdefRecord[] records = message.getRecords();
-            byte[] data = records[0].getPayload();
-            String text = new String(data, "UTF-8");
-            System.out.println("message: " + text);
-            Toast.makeText(this, "message: " + text, Toast.LENGTH_SHORT).show();
-
-            tag.close();
-            Toast.makeText(this, "connection close", Toast.LENGTH_SHORT).show();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (SmartCardException e) {
-            e.printStackTrace();
-        } catch (FormatException e) {
-            e.printStackTrace();
-        }
     }
 
     public void writeTag(NTag tag){
         try {
-            String testmessage = "This is a testing message for tag1";
-            System.out.println("here1");
-//            byte[] data = testmessage.getBytes("UTF-8");
-            System.out.println("here2");
-            NdefRecord record = NdefRecord.createTextRecord("UTF-8", testmessage);
+            String testmessage = "Test message for tag1";
+            NdefRecord record = NdefRecord.createMime(MIME_PLAIN_TEXT, testmessage.getBytes("UTF-8"));
             NdefMessage message = new NdefMessage(record);
-            System.out.println("here3");
 
             Toast.makeText(this, "connecting...", Toast.LENGTH_SHORT).show();
             tag.connect();
@@ -274,41 +206,42 @@ public class WriteNFCActivity extends ActionBarActivity {
             tag.close();
             Toast.makeText(this, "done", Toast.LENGTH_SHORT).show();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }  catch (SmartCardException e){
+        } catch (IOException | SmartCardException e) {
             e.printStackTrace();
         }
     }
 
-    /**
-     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
-     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
-     */
-//    protected static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-//        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
-//        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-//
-//        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
-//        IntentFilter[] filters = new IntentFilter[1];
-//
-//        // Notice that this is the same filter as in our manifest.
-//        filters[0] = new IntentFilter();
-//        filters[0].addAction(NfcAdapter.ACTION_TAG_DISCOVERED);
-//        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
-//        try {
-//            filters[0].addDataType(MIME_TEXT_PLAIN);
-//        } catch (IntentFilter.MalformedMimeTypeException e) {
-//            throw new RuntimeException("Check your mime type.");
-//        }
-//        adapter.enableForegroundDispatch(activity, pendingIntent, null, null);
-//    }
+    public boolean checkNFCEnabled(){
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
-    /**
-     * @param activity The corresponding {@link BaseActivity} requesting to stop the foreground dispatch.
-     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
-     */
-//    protected static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-//        adapter.disableForegroundDispatch(activity);
-//    }
+//        Stop here if device does not support NFC
+        if (nfcAdapter == null){
+            Toast.makeText(this, "This device does not support NFC.", Toast.LENGTH_LONG).show();
+            finish(); //to end this activity and return
+            return false;
+        }
+
+//        Check if NFC is enabled
+        boolean isEnabled = nfcAdapter.isEnabled();
+        if (!isEnabled) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.dialog_title_nfc_settings_fail).setMessage(R.string.dialog_message_nfc_fail);
+            builder.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish(); //to end this activity and return
+                }
+            });
+            builder.create().show();
+        }
+        return isEnabled;
+    }
+
+
 }
